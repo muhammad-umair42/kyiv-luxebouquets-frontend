39e2d8bdd9cce062e3196c8e1557e98bc601c40d
Muhammad Umair <umair42.dev@gmail.com>
2023-12-30 15:54:30 +0500

    redux persist added

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit"
-import interactionReducer from "./Slices/interactionSlice"
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: interactionReducer,
-})
+import interactionreducer from "./Slices/interactionSlice";
+import userReducer from "./Slices/userSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"], // Specify the reducers you want to persist
+};
+
+const rootReducer = combineReducers({
+  interaction: interactionreducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
-import "./index.css"
-import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
-import { store } from "./app/store.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-)
+);
